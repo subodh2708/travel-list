e98f2ee0c9636763e88e84b02aec844d816a223b
Subodh <subodhlst@gmail.com>
2025-09-26 13:16:32 +0530

    render list

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Pen", quantity: 3, packed: true },
+  { id: 3, description: "Socks", quantity: 12, packed: true },
+];
+
 export default function App() {
   return (
     <div className="app">
@@ -14,17 +20,40 @@ function Logo() {
 }
 
 function Form() {
-  return <div className="add-form">Where are you planning your trip?</div>;
+  return (
+    <div className="add-form">
+      <h3>Where are you planning your trip?</h3>
+    </div>
+  );
 }
 
 function PackingList() {
-  return <div className="list">List</div>;
+  return (
+    <div className="list">
+      <ul>
+        {initialItems.map((item) => (
+          <Item item={item} />
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
   return (
     <footer className="stats">
-      You have X items and you have already packed X (X%)
+      <em>You have X items on your list. You have already packed X (X%)</em>
     </footer>
   );
 }
